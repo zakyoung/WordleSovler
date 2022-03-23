9110d7917c03eb55647c1628f837eb976627c50d
Zakaria j Young <30213659+zakyoung@users.noreply.github.com>
2022-03-23 19:11:16 +0000

    added comments about what needs to be changed to make the program work correctly

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 """
-wordConfirmedChars = [[],[],[],[],[]]
-wordDontIncludeChars = [[],[],[],[],[]]
+once make word guess is called in the word class it should allow for input at the moment so that the user can enter what was green and what was yellow and grey and then from there it should call possible words and do the following:
+ 1. if its green make sure you only have words that have that letter at that index
+ 2. if its yellow make sure that you get rid of any words with that letter at that index and make sure you make sure that the word must include that letter at some point
+3. if its grey just call get rid of letter and add it to the list of letters that cant be used
 have to make sure that once a letter is confirmed it can never be changed from that position
 """
 class possibleWords:
@@ -40,8 +42,19 @@ class possibleWords:
       if word[index] == letter:
         wordSetNew.add(word)
     self.possibleWords = wordSetNew
-
-      
+    
+class word:
+  def __init__(self,firstWord,possibleWords):
+    self.firstWord = firstWord
+    self.possibleWords = possibleWords
+    self.finalWord = "00000"
+    self.badLetters = []
+    self.goodLetters = []
+  def solvePuzzle(self):
+    pass
+  def makeWordGuess(self,word):
+    pass
+  
 def run():
   possibleWordsReader = set()
   with open("fiveCharacterWords.txt","r") as reader:
